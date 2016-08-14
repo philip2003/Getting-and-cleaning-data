c2af3e661b83a6bc724efe1e5443087e0f588a83
Desiderius Erasmus <philip2003@users.noreply.github.com>
2016-08-14 19:25:55 +0100

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,73 +1,56 @@
-# Training Data Input
+# Stage 1
+# Input Data
 training = read.csv("UCI HAR Dataset/train/X_train.txt", sep="", header=FALSE)
 training[,562] = read.csv("UCI HAR Dataset/train/Y_train.txt", sep="", header=FALSE)
 training[,563] = read.csv("UCI HAR Dataset/train/subject_train.txt", sep="", header=FALSE)
-
-# Test Data Input
 testing = read.csv("UCI HAR Dataset/test/X_test.txt", sep="", header=FALSE)
 testing[,562] = read.csv("UCI HAR Dataset/test/Y_test.txt", sep="", header=FALSE)
 testing[,563] = read.csv("UCI HAR Dataset/test/subject_test.txt", sep="", header=FALSE)
 
-# Activity Labels
+# Input Activity Labels
 activityLabels = read.csv("UCI HAR Dataset/activity_labels.txt", sep="", header=FALSE)
 
-# Calculations
+# Input Features and Required Calculations
 features = read.csv("UCI HAR Dataset/features.txt", sep="", header=FALSE)
 features[,2] = gsub('-mean', 'Mean', features[,2])
 features[,2] = gsub('-std', 'Std', features[,2])
 features[,2] = gsub('[-()]', '', features[,2])
 
-# Combine Data
+# Combine Data Sets
 allData = rbind(training, testing)
 
-# Mean and SD for each observation
+# Calculation of Mean and SD
 cols <- grep(".*Mean.*|.*Std.*", features[,2])
 
-# Descriptive activity = activities
-
+# Descriptive activity = activities. Add column names to allData. Delete subject and activity column.
 features <- features[cols,]
 cols <- c(cols, 562, 563)
 allData <- allData[,cols]
-
-# Add the column names to allData
 colnames(allData) <- c(features$V2, "Activity", "Subject")
 colnames(allData) <- tolower(colnames(allData))
-
 currentActivity = 1
 for (currentActivityLabel in activityLabels$V2) {
         allData$activity <- gsub(currentActivity, currentActivityLabel, allData$activity)
         currentActivity <- currentActivity + 1
 }
-
 allData$activity <- as.factor(allData$activity)
 allData$subject <- as.factor(allData$subject)
-
 tidy_data = aggregate(allData, by=list(activity = allData$activity, subject=allData$subject), mean)
-
-# Delete subject and activity column.
 tidy_data[,90] = NULL
 tidy_data[,89] = NULL
 
-# Write tidy_data
+# Write and the read tidy_data
 write.table(tidy_data, "tidy_data.txt", sep="\t", row.names=FALSE)
-
 tidy_data
 
-## Create one R script called run_analysis.R that does the following:
-## 1. Merges the training and the test sets to create one data set.
-## 2. Extracts only the measurements on the mean and standard deviation for each measurement.
-## 3. Uses descriptive activity names to name the activities in the data set
-## 4. Appropriately labels the data set with descriptive activity names.
-## 5. Creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+# Stage 2
 
 if (!require("data.table")) {
   install.packages("data.table")
 }
-
 if (!require("reshape2")) {
   install.packages("reshape2")
 }
-
 require("data.table")
 require("reshape2")
 

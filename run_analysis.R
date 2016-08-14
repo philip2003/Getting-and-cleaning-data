cf9a9c4f63e4f48575aebadbd365afc356e8b10b
Desiderius Erasmus <philip2003@users.noreply.github.com>
2016-08-14 15:28:29 +0100

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,67 +1,54 @@
-require(plyr)
+# Training Data Input
+training = read.csv("UCI HAR Dataset/train/X_train.txt", sep="", header=FALSE)
+training[,562] = read.csv("UCI HAR Dataset/train/Y_train.txt", sep="", header=FALSE)
+training[,563] = read.csv("UCI HAR Dataset/train/subject_train.txt", sep="", header=FALSE)
 
-#Utils: function add suffix
-addSuffix<- function(x, suffix) {
-  if (!(x %in% c("Subject","Activity"))) {
-    paste(x,suffix, sep="")
-  }
-  else{
-    x
-  }
+# Test Data Input
+testing = read.csv("UCI HAR Dataset/test/X_test.txt", sep="", header=FALSE)
+testing[,562] = read.csv("UCI HAR Dataset/test/Y_test.txt", sep="", header=FALSE)
+testing[,563] = read.csv("UCI HAR Dataset/test/subject_test.txt", sep="", header=FALSE)
+
+# Activity Labels
+activityLabels = read.csv("UCI HAR Dataset/activity_labels.txt", sep="", header=FALSE)
+
+# Calculations
+features = read.csv("UCI HAR Dataset/features.txt", sep="", header=FALSE)
+features[,2] = gsub('-mean', 'Mean', features[,2])
+features[,2] = gsub('-std', 'Std', features[,2])
+features[,2] = gsub('[-()]', '', features[,2])
+
+# Combine Data
+allData = rbind(training, testing)
+
+# Mean and SD for each observation
+cols <- grep(".*Mean.*|.*Std.*", features[,2])
+
+# Descriptive activity = activities
+
+features <- features[cols,]
+cols <- c(cols, 562, 563)
+allData <- allData[,cols]
+
+# Add the column names to allData
+colnames(allData) <- c(features$V2, "Activity", "Subject")
+colnames(allData) <- tolower(colnames(allData))
+
+currentActivity = 1
+for (currentActivityLabel in activityLabels$V2) {
+        allData$activity <- gsub(currentActivity, currentActivityLabel, allData$activity)
+        currentActivity <- currentActivity + 1
 }
 
-#Get data
-pathfile<-file.path(getwd(),"UCI HAR Dataset")
+allData$activity <- as.factor(allData$activity)
+allData$subject <- as.factor(allData$subject)
 
-pathfiletest<-file.path(pathfile, "test")
-pathfiletrain<-file.path(pathfile, "train")
-  
-xtest<-read.table(file.path(pathfiletest,"X_test.txt"))
-ytest<-read.table(file.path(pathfiletest,"Y_test.txt"))
-subjecttest<-read.table(file.path(pathfiletest,"subject_test.txt"))
+tidy_data = aggregate(allData, by=list(activity = allData$activity, subject=allData$subject), mean)
 
-xtrain<-read.table(file.path(pathfiletrain,"X_train.txt"))
-ytrain<-read.table(file.path(pathfiletrain,"Y_train.txt"))
-subjecttrain<-read.table(file.path(pathfiletrain,"subject_train.txt"))
+# Delete subject and activity column.
+tidy_data[,90] = NULL
+tidy_data[,89] = NULL
 
-#Get activity labels 
-activitylabels<-read.table(file.path(pathfile,
-                              			"activity_labels.txt"),
-                            col.names = c("Id", "Activity")
-                            )
+# Write tidy_data
+write.table(tidy_data, "tidy_data.txt", sep="\t", row.names=FALSE)
 
-#Get features labels
-featurelabels<-read.table(file.path(pathfile,
-                            		"features.txt"),
-                            colClasses = c("character")
-                           	)
-
-#1.Merges the training and the test sets to create one data set.
-traindata<-cbind(cbind(xtrain, subjecttrain), ytrain)
-testdata<-cbind(cbind(xtest, subjecttest), ytest)
-sensordata<-rbind(traindata, testdata)
-
-sensorlabels<-rbind(rbind(featurelabels, c(562, "Subject")), c(563, "Id"))[,2]
-names(sensordata)<-sensorlabels
-
-#2. Extracts only the measurements on the mean and standard deviation for each measurement.
-sensordatameanstd <- sensordata[,grepl("mean\\(\\)|std\\(\\)|Subject|Id", names(sensordata))]
-
-#3. Uses descriptive activity names to name the activities in the data set
-sensordatameanstd <- join(sensordatameanstd, activitylabels, by = "Id", match = "first")
-sensordatameanstd <- sensordatameanstd[,-1]
-
-#4. Appropriately labels the data set with descriptive names.
-names(sensordatameanstd) <- gsub("([()])","",names(sensordatameanstd))
-#norm names
-names(sensordatameanstd) <- make.names(names(sensordatameanstd))
-
-#5. From the data set in step 4, creates a second, independent tidy data set 
-# with the average of each variable for each activity and each subject 
-finaldata<-ddply(sensordatameanstd, c("Subject","Activity"), numcolwise(mean))
-#improve column names
-finaldataheaders<-names(finaldata)
-finaldataheaders<-sapply(finaldataheaders, addSuffix, ".mean")
-names(finaldata)<-finaldataheaders
-
-write.table(finaldata, file = "sensordata_avg_by_subject.txt", row.name=FALSE)
+tidy_data
